00a47621258b1a9eb221f3217daebbde1c47cc25
Jamil <aydarkurbanbaev1406@gmail.com>
2025-04-05 19:02:13 +0500

    commit uaua

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css'
 import { useEffect } from 'react'
 import { useTelegram } from './hooks/useTelegram'
 
+const tg = window.Telegram.WebApp
+
 function App() {
 	const { onToggleButton, tg} = useTelegram()
 
@@ -11,6 +13,7 @@ function App() {
 
 	return (
   <div className='App'>
+	work
   <button onClick={onToggleButton}>toggle</button>
   </div>
   )
